509f99a1b4147c33d0fc1fe1075bff400b21a482
Martin Chabot <chabotm@gmail.com>
2012-09-10 21:52:12 -0400

    Refactor stack coloring

--- lib/rubyc/colorize_stack.rb
+++ lib/rubyc/colorize_stack.rb
@@ -2,6 +2,12 @@ module MiniTest
   # http://blog.macromates.com/2007/the-textmate-url-scheme/
   module ColorizeStack
     def write(str = "")
+      str.each_line do |line|
+        super(colorize_line(line))
+      end
+    end
+    
+    def colorize_line(line)
       red = "\e[31m"
       clear = "\e[0m"
       green = "\e[32m"
@@ -11,24 +17,17 @@ module MiniTest
       # test_0001_should_map_stdin_to_stdout(A rubyc cli) [./spec/cli_spec.rb:16]:
       # /Users/martinchabot/dev/my_gems/rubyc/spec/cli_spec.rb:16:in `test_0001_should_map_stdin_to_stdout'
       url_regex = /(.*?)([.\w\/]*.rb):(\d+)(.*)/
-      str.each_line do |line|
-        if line =~ url_regex
-          prefix = $1
-          file_name = $2
-          line_number = $3
-          complement = $4
-          if File.exist? file_name
-            full_path = File.expand_path(file_name)
-            app_trace = full_path.match(Dir.pwd) && full_path !~ /vendor/
-            new_line = "#{prefix}#{blue}txmt://open?url=file://#{File.dirname(full_path)}/#{clear}#{red if app_trace }#{File.basename(full_path)}#{clear if app_trace}&line=#{line_number
-      }#{complement}\n"
-          else
-            new_line = line
-          end
+      if line =~ url_regex
+        prefix, file_name, line_number, suffix = $1, $2, $3, $4
+        if File.exist? file_name
+          full_path = File.expand_path(file_name)
+          app_trace = full_path.match(Dir.pwd) && full_path !~ /vendor/
+          new_line = "#{prefix}#{blue}txmt://open?url=file://#{File.dirname(full_path)}/#{clear}#{red if app_trace }#{File.basename(full_path)}#{clear if app_trace}&line=#{line_number}#{suffix}\n"
         else
-           new_line = line
+          new_line = line
         end
-          super(new_line)
+      else
+         new_line = line
       end
     end
   end
